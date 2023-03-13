4324f251fb57e7ab8614b8c80fd4a2ecd60fd4ae
benson52541110 <benson89071232@gmail.com>
2023-03-14 00:26:17 +0800

    後台版型導入 50%

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import {Routes,Route} from 'react-router-dom';
 import Login from './pages/Login';
-
+import Dashboard from './pages/admin/Dashboard';
 function App() {
 
 	return (
 		<div className="App">
       <Routes>
         <Route path='/login' element={<Login/>}></Route>
+		<Route path='/admin' element={<Dashboard></Dashboard>}>
+
+		</Route>
       </Routes>
 		</div>
 	);
