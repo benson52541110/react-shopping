79803978ce6308b14e22c66f04b447aa8ff86475
benson52541110 <benson89071232@gmail.com>
2023-05-17 00:01:38 +0800

    1. 後臺功能完成 2. 套用前台版型 3. Navbar 導覽列元件 4. 前台餐點列表

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './stylesheets/all.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  axios  from 'axios';
-import { HashRouter } from 'react-router-dom'
+import axios from 'axios';
+import { HashRouter } from 'react-router-dom';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/pages/front/Products.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Pagination from '../components/Pagination';
+function Products() {
+  const [products, setProducts] = useState([]);
+  const [pagination, setPagination] = useState({});
+  const getProducts = async (page = 1) => {
+    const productRes = await axios.get(
+      `/v2/api/${process.env.REACT_APP_API_PATH}/products?page=${page}`
+    );
+    console.log(productRes);
+    setProducts(productRes.data.products);
+    setPagination(productRes.data.pagination);
+  };
+  useEffect(() => {
+    getProducts(1);
+  }, []);
+  return (
+    <>
+      <div className="container mt-md-5 mt-3 mb-7">
+        <div className="row">
+          {products.map((product) => {
+            return (
+              <div className="col-md-3" key={product.id}>
+                <div className="card border-0 mb-4 position-relative position-relative">
+                  <img
+                    src={product.imageUrl}
+                    className="card-img-top rounded-0 object-cover"
+                    alt="..."
+                  />
+                  <div className="card-body p-0">
+                    <h4 className="mb-0 mt-2">
+                      <a href="#">{product.title}</a>
+                    </h4>
+                    <p className="text-muted mt-1">{product.price}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <nav className="d-flex justify-content-center">
+          <Pagination pagination={pagination} changePage={getProducts}></Pagination>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Products;
