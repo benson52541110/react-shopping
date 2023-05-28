f947eac3776042ebcf714151d3efff6c11f8cf35
benson52541110 <benson89071232@gmail.com>
2023-05-29 00:41:16 +0800

    將每個購物車組件抽出為 cartItem

---
+++ src/pages/components/CartItem.js
@@ -0,0 +1,52 @@
+function CartItem({ item, handleCartItem, loadingItems }) {
+  const removeItem = () => handleCartItem('remove', item.id);
+  const updateItem = (e) => handleCartItem('update', item.id, item, e.target.value * 1);
+
+  return (
+    <div className="d-flex mt-4 bg-light">
+      <img
+        src={item.product.imageUrl}
+        alt=""
+        className="object-cover"
+        style={{
+          width: '120px',
+        }}
+      />
+      <div className="w-100 p-3 position-relative">
+        <button
+          type="button"
+          className="position-absolute btn"
+          style={{ top: '10px', right: '10px' }}
+          onClick={removeItem}
+        >
+          <i className="bi bi-x-lg"></i>
+        </button>
+        <p className="mb-0 fw-bold">{item.product.title}</p>
+        <p className="mb-1 text-muted" style={{ fontSize: '14px' }}>
+          {item.product.content}
+        </p>
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <div className="input-group w-50 align-items-center">
+            <select
+              name=""
+              className="form-select"
+              id=""
+              value={item.qty}
+              disabled={loadingItems.includes(item.id)}
+              onChange={updateItem}
+            >
+              {[...new Array(20)].map((_, num) => (
+                <option value={num + 1} key={num}>
+                  {num + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+          <p className="mb-0 ms-auto">NT${item.final_total}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
--- src/pages/front/Cart.js
+++ src/pages/front/Cart.js
@@ -1,38 +1,37 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { Link, useOutletContext } from 'react-router-dom';
+import CartItem from '../components/CartItem';
+
+const API_PATH = process.env.REACT_APP_API_PATH;
 
 function Cart() {
   const { cartData, getCart } = useOutletContext();
   const [loadingItems, setLoadingItem] = useState([]);
+  const [error, setError] = useState(null);
 
-  const removeCartItem = async (id) => {
+  const handleCartItem = async (action, id, item, quantity) => {
+    setError(null);
+    const api = `/v2/api/${API_PATH}/cart/${id}`;
+    let res;
     try {
-      const res = await axios.delete(`/v2/api/${process.env.REACT_APP_API_PATH}/cart/${id}`);
+      if (action === 'remove') {
+        res = await axios.delete(api);
+      } else if (action === 'update') {
+        const data = {
+          data: {
+            product_id: item.product_id,
+            qty: quantity,
+          },
+        };
+        setLoadingItem([...loadingItems, id]);
+        res = await axios.put(api, data);
+        setLoadingItem(loadingItems.filter((loadingObject) => loadingObject !== id));
+      }
       console.log(res);
       getCart();
     } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const updateCartItem = async (item, quantity) => {
-    const data = {
-      data: {
-        product_id: item.product_id,
-        qty: quantity,
-      },
-    };
-    setLoadingItem([...loadingItems, item.id]);
-    try {
-      const res = await axios.put(
-        `/v2/api/${process.env.REACT_APP_API_PATH}/cart/${item.id}`,
-        data
-      );
-      console.log(res);
-      setLoadingItem(loadingItems.filter((loadingObject) => loadingObject !== item.id));
-      getCart();
-    } catch (error) {
+      setError(error.message || 'Failed to update cart.');
       console.log(error);
     }
   };
@@ -41,6 +40,7 @@ function Cart() {
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-6 bg-white py-5 full-height">
+          {error && <p>{error}</p>} {/* display error message */}
           {cartData?.carts?.length === 0 ? (
             <>
               <div className="alert alert-secondary">還沒有選擇餐點喔</div>
@@ -54,59 +54,15 @@ function Cart() {
                 <h2 className="mt-2">您的餐點</h2>
               </div>
 
-              {cartData?.carts?.map((item) => {
-                return (
-                  <div className="d-flex mt-4 bg-light" key={item.id}>
-                    <img
-                      src={item.product.imageUrl}
-                      alt=""
-                      className="object-cover"
-                      style={{
-                        width: '120px',
-                      }}
-                    />
-                    <div className="w-100 p-3 position-relative">
-                      <button
-                        type="button"
-                        className="position-absolute btn"
-                        style={{ top: '10px', right: '10px' }}
-                        onClick={() => {
-                          removeCartItem(item.id);
-                        }}
-                      >
-                        <i className="bi bi-x-lg"></i>
-                      </button>
-                      <p className="mb-0 fw-bold">{item.product.title}</p>
-                      <p className="mb-1 text-muted" style={{ fontSize: '14px' }}>
-                        {item.product.content}
-                      </p>
-                      <div className="d-flex justify-content-between align-items-center w-100">
-                        <div className="input-group w-50 align-items-center">
-                          <select
-                            name=""
-                            className="form-select"
-                            id=""
-                            value={item.qty}
-                            disabled={loadingItems.includes(item.id)}
-                            onChange={(e) => {
-                              updateCartItem(item, e.target.value * 1);
-                            }}
-                          >
-                            {[...new Array(20)].map((i, num) => {
-                              return (
-                                <option value={num + 1} key={num}>
-                                  {num + 1}
-                                </option>
-                              );
-                            })}
-                          </select>
-                        </div>
-                        <p className="mb-0 ms-auto">NT${item.final_total}</p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
+              {cartData?.carts?.map((item) => (
+                <CartItem
+                  key={item.id}
+                  item={item}
+                  handleCartItem={handleCartItem}
+                  loadingItems={loadingItems}
+                />
+              ))}
+
               <div className="d-flex justify-content-between mt-4">
                 <p className="mb-0 h4 fw-bold">總金額</p>
                 <p className="mb-0 h4 fw-bold">NT${cartData.final_total}</p>
